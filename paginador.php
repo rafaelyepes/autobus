9c6a72b3393327ab5a49caca52cfec24dba7fd07
Rafael Yepes <rafael.yepes@lacroixmeats.com>
2018-11-19 10:20:11 -0500

    version 2

--- paginador.php
+++ paginador.php
@@ -1,6 +1,61 @@
 <table class="tablat" border=1 id ="your-table-id" cellspacing=0 cellpadding=0>
 <?php
 include ("./conectar4.php");
+
+$tipoc="";
+//header("Content-Type: text/html;charset=utf-8");
+include ("./conectar4.php");
+$codigo="plantill011a.php";
+$RegistrosAMostrar=25;
+
+//  $fechai=($_GET['c']);
+//  $fechaf=($_GET['d']);
+
+//  $tipoc="0";
+
+//estos valores los recibo por GET
+if(isset($_GET['pag'])){
+  $RegistrosAEmpezar=($_GET['pag']-1)*$RegistrosAMostrar;
+  $PagAct=$_GET['pag'];
+//caso contrario los iniciamos
+}else{
+  $RegistrosAEmpezar=0;
+  $PagAct=1;
+
+}
+
+$nombre_fichero1="../plantilla/archivos/fdc/";
+
+if(isset($_GET['b'])) {
+    $codigo = $_GET["b"];
+}
+if(isset($_GET['c'])) {
+    $fechai = $_GET["c"];
+}
+if(isset($_GET['d'])) {
+    $fechaf = $_GET["d"];
+}
+
+if(isset($_GET['tipoc'])) {
+    $tipoc= $_GET["tipoc"];
+}
+
+if ($tipoc!="0") {
+  $fechai = date('Y-m-d');
+  $fechaf = date('Y-m-d');
+  if ($tipoc == "1"){
+    $fechai = date('Y-m-d', strtotime('-720 day'));
+  }
+  if ($tipoc == "2"){
+    $fechai = date('Y-m-d', strtotime('-8 day'));
+  }
+  if ($tipoc == "3"){
+    $fechai = date('Y-m-d', strtotime('-30 day'));
+  }
+  if ($tipoc == "4"){
+    $fechai = date('Y-m-d', strtotime('-360 day'));
+  }
+}
 $fechai="2018-09-01";
 $fechaf="2018-11-30";
 $i=0;
